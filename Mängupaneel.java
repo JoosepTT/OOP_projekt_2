c05fcb1eda49816993aa3477ef3dc6986aa48653
Joosep Tiger Tilgar <jooseptiger@ut.ee>
2025-05-22 13:14:40 +0300

    Update Mängupaneel.java

--- Mängupaneel.java
+++ Mängupaneel.java
@@ -1,20 +1,29 @@
 package com.example.oop_rt2;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class Mängupaneel extends Pane implements Runnable {
-
     public static final int laius = 1280;
     public static final int kõrgus = 720;
     public static final int fps = 60;
     Thread mänguLõim;
+    public static int mängijaid = 1; // vaikimisi üksikmäng
+    public static String mangijaNimi;
 
     private Canvas lõuend;
     private GraphicsContext gc;
-    private Mänguhaldur mh;
+    private Mänguhaldur mh1;
+    private Mänguhaldur mh2;
     private Stage peaLava;
 
     public void setPeaLava(Stage lava) {
@@ -28,11 +37,22 @@ public class Mängupaneel extends Pane implements Runnable {
 
         lõuend = new Canvas(laius, kõrgus);
         gc = lõuend.getGraphicsContext2D();
-        this.getChildren().add(lõuend);
+        getChildren().add(lõuend);
 
-        mh = new Mänguhaldur();
+        if (mängijaid == 1) {
+            mh1 = new Mänguhaldur(laius / 3, true);
+            käivitaMäng();
 
-        käivitaMäng();
+        } else {
+            // Mõlemad mängud
+            mh1 = new Mänguhaldur(0, true); // Mängija 1 vasakul
+            mh2 = new Mänguhaldur(laius / 2, false); // Mängija 2 paremal
+
+            mh1.setVastane(mh2);
+            mh2.setVastane(mh1);
+
+            käivitaMäng();
+        }
     }
 
     public void käivitaMäng() {
@@ -48,7 +68,7 @@ public class Mängupaneel extends Pane implements Runnable {
         long viimaneAeg = System.nanoTime();
         long praeguneAeg;
 
-        while (mänguLõim != null && !mh.kasMängOnLäbi) {
+        while (mänguLõim != null && !mh1.kasMängOnLäbi && (mh2 == null || !mh2.kasMängOnLäbi)) {
             praeguneAeg = System.nanoTime();
             delta += (praeguneAeg - viimaneAeg) / joonistusIntervall;
             viimaneAeg = praeguneAeg;
@@ -61,12 +81,18 @@ public class Mängupaneel extends Pane implements Runnable {
                 delta--;
             }
         }
+
+        // Kui mäng lõppes
+        javafx.application.Platform.runLater(() -> {
+            kuvaLõpuAken();
+        });
     }
 
     // mänguinfo uudendatakse siis, kui mäng pole pausil
     private void uuendaInfot() {
-        if (Klahvihaldur.pausSees == false && mh.kasMängOnLäbi == false) {
-            mh.uuenda();
+        if (!Klahvihaldur.pausSees && !mh1.kasMängOnLäbi && (mh2 == null || !mh2.kasMängOnLäbi)) {
+            mh1.uuenda();
+            if (mh2 != null) mh2.uuenda();
         }
         /*
         else {
@@ -78,8 +104,55 @@ public class Mängupaneel extends Pane implements Runnable {
     }
 
     private void kuvaKomponent() {
-        gc.clearRect(0, 0, laius, kõrgus); // lõuend tühjendatakse
-        mh.joonista(gc); // mängu elementide kuvamine
+        gc.clearRect(0, 0, laius, kõrgus);
+
+        // Keskelt poolitav joon
+        gc.setStroke(Color.WHITE);
+        gc.setLineWidth(2);
+        gc.strokeLine(laius / 2, 0, laius / 2, kõrgus);
+
+        // Mängud
+        mh1.joonista(gc);
+        if (mh2 != null) mh2.joonista(gc);
+
+        // Kirjeldused
+        gc.setFill(Color.WHITE);
+        gc.setFont(new Font(20));
+        gc.fillText("Mängija 1 (WASD)", laius / 4 - 80, 30);
+        gc.fillText("Mängija 2 (Nooled)", 3 * laius / 4 - 100, 30);
+    }
+
+    private void kuvaLõpuAken() {
+        VBox lõppPaneel = new VBox(15);
+        lõppPaneel.setStyle("-fx-background-color: black;");
+        lõppPaneel.setPrefSize(laius, kõrgus);
+        lõppPaneel.setAlignment(Pos.CENTER);
+
+        Label lõppTekst;
+
+        if (mängijaid == 1 && mangijaNimi != null) {
+            int mängijaSkoor = mh1.getSkoor();
+            Menüü.uuendaSkoori(mangijaNimi, mängijaSkoor);
+            lõppTekst = new Label("Mäng on lõppenud!\nSinu skoor: " + mängijaSkoor);
+        } else {
+            lõppTekst = new Label("Mäng on lõppenud!");
+        }
+
+        lõppTekst.setFont(new Font(30));
+        lõppTekst.setTextFill(Color.WHITE);
+
+        Button tagasiBtn = new Button("Naase menüüsse");
+        tagasiBtn.setOnAction(e -> {
+            try {
+                new Menüü().start(peaLava);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        lõppPaneel.getChildren().addAll(lõppTekst, tagasiBtn);
+        Scene lõpuStseen = new Scene(lõppPaneel, laius, kõrgus);
+        peaLava.setScene(lõpuStseen);
     }
 
 }
