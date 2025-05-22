f0512e43318096b915a035d814ee001155a0368b
Joosep Tiger Tilgar <jooseptiger@ut.ee>
2025-05-22 13:14:12 +0300

    Update Mänguhaldur.java

--- Mänguhaldur.java
+++ Mänguhaldur.java
@@ -6,7 +6,6 @@ import javafx.scene.text.Font;
 import java.util.ArrayList;
 import java.util.Random;
 
-
 // Mänguhaldur:
 // - kuvab mänguala
 // - haldab mänuplokkidega seonduvat
@@ -17,9 +16,13 @@ public class Mänguhaldur {
     final int laius = 360;
     final int kõrgus = 600;
 
-    public static int vasak_x;
-    public static int parem_x;
-    public static int ülemine_y;
+    private final boolean mängija1;
+    private final int abiX;
+
+    // mängija ala
+    public int vasak_x;
+    public int parem_x;
+    public int ülemine_y;
     public static int alumine_y;
 
     // klotsid
@@ -29,29 +32,57 @@ public class Mänguhaldur {
     Klotsid järgmineKlots;
     final int järgmiseKlotsi_x;
     final int järgmiseKlotsi_y;
-    public static ArrayList<Plokk> staatilisedPlokid = new ArrayList<>(); // massiiv, kuhu pannakse mitteaktiivsed klotsid (mis on juba omal kohal)
 
     // muud
-    public static int langemisIntervall = 60; // plokk kukub iga raami järel madalamale
+    public ArrayList<Plokk> staatilisedPlokid;
+    public static int langemisIntervall = 30;
     public boolean kasMängOnLäbi;
+    private Mänguhaldur vastane;
 
     // skoorid
     int tase = 1;
     int jooni;
     int skoor;
 
-    public Mänguhaldur() {
+    public int getSkoor() {
+        return skoor; // eeldusel, et sul on skoor muutujana klassis olemas
+    }
+
+    public void setVastane(Mänguhaldur vastane) {
+        this.vastane = vastane;
+    }
+
+    public Mänguhaldur(int abiX, boolean mängija1) {
+        this.mängija1 = mängija1;
+        if (abiX == 0) {
+            this.abiX = abiX;
+            vasak_x = (Mängupaneel.laius / 2) - (laius / 2);
+        } else {
+            this.abiX = abiX;
+            vasak_x = abiX + 50;
+        }
+
+        this.staatilisedPlokid = new ArrayList<>();
         // mänguala positsioon
-        vasak_x = (Mängupaneel.laius / 2) - (laius / 2);
         parem_x = vasak_x + laius;
         ülemine_y = 50;
         alumine_y = ülemine_y + kõrgus;
 
+        // klotside alguspositsioon
         klotsiAlgus_x = vasak_x + (laius / 2) - Plokk.plokiSuurus;
         klotsiAlgus_y = ülemine_y + Plokk.plokiSuurus;
 
-        järgmiseKlotsi_x = parem_x + 175;
-        järgmiseKlotsi_y = ülemine_y + 500;
+        // järgmise klotsi eelvaade
+        if (abiX == 0) {
+            järgmiseKlotsi_x = parem_x + 175;
+        } else { // kui on duellirežiim
+            if (mängija1) {
+                järgmiseKlotsi_x = parem_x + 50;
+            } else {
+                järgmiseKlotsi_x = abiX + 460;
+            }
+        }
+        järgmiseKlotsi_y = ülemine_y + 150;
 
         // järgmise klotsi seadistamine
         praeguneKlots = valiKlots();
@@ -63,28 +94,30 @@ public class Mänguhaldur {
     // suvalise klotsi valimine ja loomine
     private Klotsid valiKlots() {
         Klotsid klots = null;
-        int i = new Random().nextInt(7); // klotsi valimine
+        int i = new Random().nextInt(7);
 
         switch (i) {
-            case 0: klots = new Kujund_1();break;
-            case 1: klots = new Kujund_2();break;
-            case 2: klots = new Kujund_3();break;
-            case 3: klots = new Kujund_4();break;
-            case 4: klots = new Kujund_5();break;
-            case 5: klots = new Kujund_6();break;
-            case 6: klots = new Kujund_7();break;
+            case 0: klots = new Kujund_1(mängija1); break;
+            case 1: klots = new Kujund_2(mängija1); break;
+            case 2: klots = new Kujund_3(mängija1); break;
+            case 3: klots = new Kujund_4(mängija1); break;
+            case 4: klots = new Kujund_5(mängija1); break;
+            case 5: klots = new Kujund_6(mängija1); break;
+            case 6: klots = new Kujund_7(mängija1); break;
         }
         return klots;
     }
 
     public void uuenda() {
         // kontrollitakse, kas parasjagu kuvatav klots on endiselt aktiivne
-        if (praeguneKlots.aktiivne == false) {
+        if (!praeguneKlots.aktiivne) {
             // kui klots pole aktiivne, siis see tõstetakse ümber staatiliste klotside massiivi
-            staatilisedPlokid.add(praeguneKlots.a[0]);
-            staatilisedPlokid.add(praeguneKlots.a[1]);
-            staatilisedPlokid.add(praeguneKlots.a[2]);
-            staatilisedPlokid.add(praeguneKlots.a[3]);
+            for (Plokk plokk : praeguneKlots.a) {
+                staatilisedPlokid.add(new Plokk(plokk.värv) {{
+                    x = plokk.x;
+                    y = plokk.y;
+                }});
+            }
 
             // kontroll, kas mäng on läbi saanud
             if (praeguneKlots.a[0].x == klotsiAlgus_x && praeguneKlots.a[0].y == klotsiAlgus_y) {
@@ -98,13 +131,13 @@ public class Mänguhaldur {
             // kui üks klots on paigas, siis praegune klots asendatakse järgmisega
             praeguneKlots = järgmineKlots;
             praeguneKlots.määraXY(klotsiAlgus_x, klotsiAlgus_y);
-            järgmineKlots = valiKlots(); // omakorda uuele klotsile järgneva valimine
+            järgmineKlots = valiKlots();
             järgmineKlots.määraXY(järgmiseKlotsi_x, järgmiseKlotsi_y);
 
             // pärast aktiivse klotsi deaktiveerimist kontrollitakse, kas on rida, mida kustutada
             kasSaabKustutada();
-
         } else { // vastasel juhul uuendadatkse klotsi parameetreid
+            praeguneKlots.kontrolliLiikumisPõrget(staatilisedPlokid, vasak_x, parem_x, alumine_y);
             praeguneKlots.uuenda();
         }
     }
@@ -166,30 +199,44 @@ public class Mänguhaldur {
         if (joonteArv > 0) {
             int jooneSkoor = 10 * tase;
             skoor += jooneSkoor * joonteArv;
+
+            if (vastane != null) {
+                vastane.lisaSegavRida();
+            }
         }
     }
 
+    public void lisaSegavRida() {
+        // Tõsta kõik plokid ühe võrra üles
+        for (Plokk plokk : staatilisedPlokid) {
+            plokk.y -= Plokk.plokiSuurus;
+            // kui plokk liigub üleval äärest välja, siis on mäng läbi
+            if (plokk.y < ülemine_y) {
+                kasMängOnLäbi = true;
+            }
+        }
+
+        // Loo uus juhuslik alumine rida
+        int startX = vasak_x;
+        int y = alumine_y - Plokk.plokiSuurus;
+        Random rnd = new Random();
+
+        for (int i = 0; i < 12; i++) {
+            if (rnd.nextBoolean()) { // 50% tõenäosus
+                Plokk uusPlokk = new Plokk(Color.GRAY);
+                uusPlokk.x = startX + i * Plokk.plokiSuurus;
+                uusPlokk.y = y;
+                staatilisedPlokid.add(uusPlokk);
+            }
+        }
+    }
+
+
     public void joonista(GraphicsContext gc) {
-
         // mänguala kuvamine
-        gc.setStroke(Color.WHITE);
+        gc.setStroke(mängija1 ? Color.ORANGE : Color.CYAN);
         gc.setLineWidth(4);
-        gc.strokeRect(vasak_x - 4, ülemine_y - 4, laius + 8, kõrgus + 8);
-
-        // plokkide eelvaade
-        int x = parem_x + 100;
-        int y = alumine_y - 200;
-        gc.strokeRect(x, y, 200, 200);
-
-        // skoori kuvamine
-        gc.strokeRect(x, ülemine_y, 250, 300);
-        x += 40;
-        y = ülemine_y + 90;
-        gc.fillText("Tase: " + tase, x, y);
-        y += 70;
-        gc.fillText("Ridu: " + jooni, x, y);
-        y += 70;
-        gc.fillText("Skoor: " + skoor, x, y);
+        gc.strokeRect(vasak_x - 2, ülemine_y - 2, laius + 4, kõrgus + 4);
 
         // praeguse klotsi kuvamine
         if (praeguneKlots != null) {
@@ -197,18 +244,29 @@ public class Mänguhaldur {
         }
 
         // järgmisena tuleva klotsi kuvamine
+        gc.setStroke(Color.WHITE);
+        gc.strokeRect(järgmiseKlotsi_x - 37, järgmiseKlotsi_y - 37,
+                Plokk.plokiSuurus * 4 - 20, Plokk.plokiSuurus * 4 - 20);
         järgmineKlots.kuva(gc);
 
         // staatiliste klotside kuvamine
-        for (int i = 0; i < staatilisedPlokid.size(); i++) {
-            staatilisedPlokid.get(i).kuva(gc);
+        for (Plokk plokk : staatilisedPlokid) {
+            plokk.kuva(gc);
         }
+
+        // skoori kuvamine
+        int scoreX = mängija1 ? parem_x + 20 : abiX + 430;
+        gc.setFill(Color.WHITE);
+        gc.setFont(new Font(20));
+        gc.fillText("Player " + (mängija1 ? "1" : "2"), scoreX, ülemine_y + 30);
+        gc.fillText("Score: " + skoor, scoreX, ülemine_y + 60);
+        gc.fillText("Lines: " + jooni, scoreX, ülemine_y + 90);
 
         // mängu lõpetamine ja pausile panemine
         gc.setFill(Color.YELLOW);
         gc.setFont(Font.font("Arial", 50));
-        x = vasak_x + 70;
-        y = parem_x + 320;
+        int x = vasak_x + 70;
+        int y = ülemine_y + 320;
         if (kasMängOnLäbi) {
             gc.fillText("MÄNG ON LÄBI", x ,y);
         } else if (Klahvihaldur.pausSees) {
@@ -216,7 +274,6 @@ public class Mänguhaldur {
         }
 
         // mängu pealkirja kuvamine
-
     }
 
 }
